609aec9d90ee80ddaa2c49685c936f6da6365ec5
Komall03 <sachin2000k@gmail.com>
2022-12-05 03:16:25 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a wafer project using ML ops',
+    author='sachin2000k',
+    license='MIT',
+)
